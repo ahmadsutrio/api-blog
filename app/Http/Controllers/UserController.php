6f3658bc1401ae3cecc63f6b43f11dda58e9535f
ahmadsutrio <ahmadsutrio.dev@gmail.com>
2026-05-05 00:22:09 +0700

    add: add api sanctum, route register,login,logout, and  model, controller, request ;

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validate($request->rules());
+        User::create($data);
+
+        return response()->json([
+            'status' => true,
+            'code' => 201,
+            'message' => 'user berhasil didaftarkan'
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validate($request->rules());
+         $user = User::where('email','=', $data['email'],false)->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'status' => false,
+                'code' => 401,
+                'message' => 'email atau password salah'
+            ], 401);
+        }
+
+        $token = $user->createToken('user_login')->plainTextToken;
+
+        return response()->json([
+            'status' => true,
+            'code' => 200,
+            'token' => $token,
+            'data' => [
+                'username' => $user->username,
+                'email' => $user->email,
+            ]
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        // $request->user()->currentAccessToken()->delete();
+        $request->user()->tokens()->delete();
+        return response()->json(
+            [
+                'status' => true,
+                'code' => 200,
+                'message' => 'berhasil logout'
+            ],
+            200
+        );
+    }
+}
---
+++ app/Models/Posts.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Posts extends Model
+{
+    protected $table = "posts";
+    public $fillable =[
+        'title','slug','category','content','image','created_at','updated_at','deleted_at'
+    ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+}
